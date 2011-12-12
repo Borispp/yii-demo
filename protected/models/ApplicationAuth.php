ddae0e1bc6b760b4fb90bb840bccd6f79f4e1f71
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2011-12-12 12:27:18 +0200

    Client authorization finished

--- protected/models/ApplicationAuth.php
+++ protected/models/ApplicationAuth.php
@@ -3,7 +3,7 @@
  * This is the model class for table "application_auth".
  *
  * The followings are the available columns in table 'option_group':
-  `type` set('client','proofing') NOT NULL DEFAULT 'client',
+`type` set('client','proofing') NOT NULL DEFAULT 'client',
 
  * @property integer $id
  * @property string $device_id
@@ -39,12 +39,12 @@ class ApplicationAuth extends YsaActiveRecord
 		return array();
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
-//		return array(
-//			array('rank, hidden', 'numerical', 'integerOnly'=>true),
-//			array('title, slug', 'length', 'max'=>100),
-//			array('slug', 'unique'),
-//			array('slug, title', 'required'),
-//		);
+		//		return array(
+		//			array('rank, hidden', 'numerical', 'integerOnly'=>true),
+		//			array('title, slug', 'length', 'max'=>100),
+		//			array('slug', 'unique'),
+		//			array('slug, title', 'required'),
+		//		);
 	}
 
 	/**
@@ -53,7 +53,7 @@ class ApplicationAuth extends YsaActiveRecord
 	public function relations()
 	{
 		return array(
-			//'option' => array(self::HAS_MANY, 'Option', 'group_id'),
+			'application'	=> array(self::HAS_ONE, 'Application', 'app_id')
 		);
 	}
 
@@ -65,10 +65,70 @@ class ApplicationAuth extends YsaActiveRecord
 		return array(
 			'id'		=> 'ID',
 			'device_id'	=> 'Device ID',
-			'app_id'	=> 'Application ID',
 			'token'		=> 'Secure Token',
 			'state'		=> 'State',
 			'type'		=> 'Auth type'
 		);
 	}
+
+	/**
+	 * @param string $password
+	 * @param integer $deviceId
+	 * @param integer $appId
+	 * @return string
+	 */
+	protected function _generateToken($password, $deviceId, Application $obApplication)
+	{
+		return md5(md5($password.$deviceId.$obApplication->id).Yii::app()->params['salt']);
+	}
+
+	/**
+	 * Authorization by password.
+	 * Returns token or bool FALSE.
+	 *
+	 * @param string $password
+	 * @param string $appKey
+	 * @param string $deviceId
+	 * @param string $type
+	 * @return mixed
+	 */
+	public function authByPassword($password, $appKey, $deviceId, $type)
+	{
+		$obApplication = Application::model()->findByKey($appKey);
+		if (!$obApplication)
+			return FALSE;
+		if ($this->authByToken($token = $this->_generateToken($password, $deviceId, $obApplication), $appKey, $deviceId, $type))
+			return $token;
+		if ($obApplication->passwd != $password)
+			return FALSE;
+		$model = new ApplicationAuth();
+		$model->token = $token;
+		$model->app_id = $obApplication->id;
+		$model->device_id = $deviceId;
+		$model->state = 1;
+		$model->type = $type;
+		$model->save();
+		return $token;
+	}
+
+	/**
+	 * @param string $token
+	 * @param string $appKey
+	 * @param string $deviceId
+	 * @param string $type
+	 * @return bool
+	 */
+	public function authByToken($token, $appKey, $deviceId, $type)
+	{
+		$obApplication = Application::model()->findByKey($appKey);
+		if (!$obApplication)
+			return FALSE;
+		return $this->findByAttributes(array(
+			'app_id'	=> $obApplication->id,
+			'token'		=> $token,
+			'device_id'	=> $deviceId,
+			'type'		=> $type,
+		));
+	}
+
 }
