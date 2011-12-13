d02f7c514bce5116f7e1bc5905e90d595bb08a9e
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2011-12-13 17:28:44 +0200

    spaces to tabs

--- protected/modules/admin/views/discount/index.php
+++ protected/modules/admin/views/discount/index.php
@@ -1,41 +1,41 @@
 <div class="g12">
-    <?php $this->beginWidget('YsaAdminForm', array(
-        'id'=>'membership-form',
-        'action'=>Yii::app()->createUrl('/admin/discount/delete'),
-        'method'=>'post',
-    )); ?>
-    
-    <p>
-        <?php echo CHtml::link('Add New', array('add'), array('class' => 'btn i_plus icon ysa fr')); ?>
-        <span class="clearfix"></span>
-    </p>
-    
-    <table class="data">
-        <thead>
-            <tr>
-                <th class="w_1"><input type="checkbox" value="" class="ids-toggle" /></th>
-                <th class="l">Code</th>
-                <th class="w_20">Summ</th>
-                <th class="w_10">&nbsp;</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($entries as $entry) : ?>
-                <tr>
-                    <td><input type="checkbox" class="del" value="<?php echo $entry->id; ?>" name="ids[]" /></td>
-                    <td class="l">
-                        <?php echo CHtml::link($entry->code, array('edit', 'id' => $entry->id)); ?>
-                    </td>
-                    <td><?php echo $entry->summ ?>%</td>
-                    <td>
-                        <?php echo CHtml::link('Edit', array('edit', 'id' => $entry->id), array('class' => 'btn small blue')); ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <p>
-        <button class="submit small red icon i_recycle delete-entries">Delete Selected</button>
-    </p>
-    <?php $this->endWidget(); ?>
+	<?php $this->beginWidget('YsaAdminForm', array(
+		'id'=>'membership-form',
+		'action'=>Yii::app()->createUrl('/admin/discount/delete'),
+		'method'=>'post',
+	)); ?>
+
+	<p>
+		<?php echo CHtml::link('Add New', array('add'), array('class' => 'btn i_plus icon ysa fr')); ?>
+		<span class="clearfix"></span>
+	</p>
+
+	<table class="data">
+		<thead>
+		<tr>
+			<th class="w_1"><input type="checkbox" value="" class="ids-toggle" /></th>
+			<th class="l">Code</th>
+			<th class="w_20">Summ</th>
+			<th class="w_10">&nbsp;</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($entries as $entry) : ?>
+		<tr>
+			<td><input type="checkbox" class="del" value="<?php echo $entry->id; ?>" name="ids[]" /></td>
+			<td class="l">
+				<?php echo CHtml::link($entry->code, array('edit', 'id' => $entry->id)); ?>
+			</td>
+			<td><?php echo $entry->summ ?>%</td>
+			<td>
+				<?php echo CHtml::link('Edit', array('edit', 'id' => $entry->id), array('class' => 'btn small blue')); ?>
+			</td>
+		</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<p>
+		<button class="submit small red icon i_recycle delete-entries">Delete Selected</button>
+	</p>
+	<?php $this->endWidget(); ?>
 </div>
