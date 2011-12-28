f6ac13688c0317b1499b5b725155e7c1e055b99d
Eugen Figursky <eugen@flosites.com>
2011-12-28 12:15:49 +0200

    added user orders

--- protected/components/YsaController.php
+++ protected/components/YsaController.php
@@ -167,6 +167,7 @@ class YsaController extends CController
 				array('label'=>'Studio', 'url'=>array('studio/')),
 				array('label'=>'Events', 'url'=>array('event/')),
 				array('label'=>'Portfolio', 'url'=>array('portfolio/')),
+				array('label'=>'Orders', 'url'=>array('order/')),
 				array('label'=>'Settings', 'url'=>array('settings/')),
 			);
 		}
