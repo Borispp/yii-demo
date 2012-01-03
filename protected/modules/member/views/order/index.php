2bf3a7b2892983a83939938fcc335d7120d77e8a
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2012-01-03 16:25:11 +0200

    Order functional

---
+++ protected/modules/member/views/order/index.php
@@ -0,0 +1,33 @@
+<?php echo YsaHtml::pageHeaderTitle('Orders'); ?>
+<section class="body w" id="inbox-block">
+	<?php if (count($entries)):?>
+	<table class="data">
+		<thead>
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+			<th>Date</th>
+			<th>Link to PDF</th>
+		</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($entries as $entry) : ?>
+		<tr>
+			<td>
+				<?php echo $entry->last_name?>
+			</td>
+			<td>
+				<?php echo $entry->email?>
+			</td>
+			<td><?php echo date('m.d.Y H:i', strtotime($entry->created)) ?></td>
+			<td><?php echo CHtml::link('PDF', $entry->getPdfUrl(), array('target' => '_blank')); ?></td>
+		</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<? $this->widget('YsaAdminPager',array('pages'=>$pagination)) ?>
+	<?php else:?>
+	<h3>No messages yet</h3>
+	<?php endif?>
+</section>
