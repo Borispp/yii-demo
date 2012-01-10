2e03f9bc570ddd1375546b8a9bb500ea62bf61f3
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2012-01-10 12:37:32 +0200

    api defaul controllers notifications

--- protected/modules/api/controllers/DefaultController.php
+++ protected/modules/api/controllers/DefaultController.php
@@ -8,13 +8,12 @@ class DefaultController extends YsaApiController
 	 */
 	public function actionIndex()
 	{
-		$eventResult = Event::model()->findByAttributes(array(
-			'type'	=> array('portfolio','proof'),
-			'state'	=> 1
-		));
 		$this->render('index', array(
 			'applicationList'	=> Application::model()->findAll(),
-			'eventList'			=> is_array($eventResult) ? $eventResult : array($eventResult)
+			'eventList'			=> Event::model()->findAllByAttributes(array(
+				'type'	=> array('portfolio','proof'),
+				'state'	=> 1
+			))
 		));
 	}
 }
