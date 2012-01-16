52d785c2ccc6489d0802485161794a84b8fb661d
George Mihailov <mihailoff@gmail.com>
2012-01-16 14:50:40 +0200

    Created separate notification bar widget and added it to login page

--- protected/views/layouts/member.php
+++ protected/views/layouts/member.php
@@ -9,19 +9,8 @@
 		'links' => $this->breadcrumbs,
 	)); ?>
 
-	<?php if(Yii::app()->user->hasFlash('success')): ?>
-		<div class="flash success">
-			<?php echo Yii::app()->user->getFlash('success'); ?>
-		</div>
-	<?php elseif (Yii::app()->user->hasFlash('notice')): ?>
-		<div class="flash notice">
-			<?php echo Yii::app()->user->getFlash('notice'); ?>
-		</div>
-	<?php elseif (Yii::app()->user->hasFlash('error')): ?>
-		<div class="flash error">
-			<?php echo Yii::app()->user->getFlash('success'); ?>
-		</div>
-	<?php endif;?>
+	<?php $this->widget('YsaNotificationBar'); ?>
+	
 </div>
 
 <?php echo $content; ?>
---
+++ protected/widgets/YsaNotificationBar.php
@@ -0,0 +1,33 @@
+<?php
+class YsaNotificationBar extends CWidget
+{
+	public function run()
+	{
+		$msg = '';
+		$app = Yii::app();
+		
+		if($app->user->hasFlash('success'))
+		{
+			$msg = $this->_render($app, 'success');
+		}
+		elseif ($app->user->hasFlash('notice'))
+		{
+			$msg = $this->_render($app, 'notice');
+		}
+		elseif ($app->user->hasFlash('error'))
+		{
+			$msg = $this->_render($app, 'error');
+		}
+		
+		echo $msg;
+	}
+	
+	protected function _render( $app, $msg_type )
+	{
+		return <<<"MSG"
+			<div class="flash {$msg_type}">
+				{$app->user->getFlash($msg_type)}
+			</div>
+MSG;
+	}
+}
