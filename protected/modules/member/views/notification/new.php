7e20aba7f4887b8a352a76f511cef2510b16c9fd
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2012-01-20 18:11:13 +0200

    notification ajax-form template

--- protected/modules/member/views/notification/new.php
+++ protected/modules/member/views/notification/new.php
@@ -1,62 +1,19 @@
-<style type="text/css">
-		#notification-events,
-		#notification-clients
-		{
-			background: #ccc;
-			width: 100px;
-			height: 100px;
-		}
-		#events,
-		#clients
-		{
-			background: #0cc;
-			width: 100px;
-			height: 100px;
-		}
-	</style>
-<div class="w">
+<div class="form">
 	<script type="text/javascript" src="<?php echo Yii::app()->baseUrl?>/resources/js/member/notification.js"></script>
-	<div class="form">
-	<?php $form=$this->beginWidget('YsaMemberForm', array(
-		'id'=>'notification-form',
-		'enableAjaxValidation'=>false,
-	)); ?>
-	<section>
-	</section>
-	<section>
-		<?php echo $form->labelEx($entry,'message'); ?>
-		<div>
-			<?php echo $form->textArea($entry,'message', array('cols' => 40, 'rows' => 4)); ?>
-			<?php echo $form->error($entry,'message'); ?>
-		</div>
-	</section>
-	<?php echo $form->hiddenField($entry,'events');?>
-	<?php echo $form->hiddenField($entry,'clients');?>
-		
-	<section id="notification-events" rel="client-<?php echo $entry->id?>">
-	</section>
+	<form method="post" action="<?php echo Yii::app()->createAbsoluteUrl('/member/notification/send')?>" id="new-notification-form">
+		<input type="hidden" name="type" value="<?php echo $type?>"/>
+		<input type="hidden" name="recipient" value="<?php echo $recipient?>"/>
+		<section>
+			<label for="message"></label>
+			<div>
+				<textarea name="message" id="message" class="required" cols="40" rows="4"></textarea>
+			</div>
+		</section>
+		<section class="button">
+			<input type="submit" value="Send"/>
+		</section>
+	</form>
+	<section id="response-message">
 
-	<section id="events">
-		<ul>
-			<?php foreach($events as $obEvent):?>
-			<li class="event" id="event-<?php echo $obEvent->id?>"><?php echo $obEvent->name?></li>
-			<?php endforeach?>
-		</ul>
-	</section>
-
-	<section id="notification-clients" rel="client-<?php echo $entry->id?>">
-	</section>
-
-	<section id="clients">
-		<ul>
-			<?php foreach($clients as $obClient):?>
-			<li class="client" id="client-<?php echo $obClient->id?>"><?php echo $obClient->name?></li>
-			<?php endforeach?>
-		</ul>
-	</section>
-	<section class="button">
-		<?php echo YsaHtml::submitButton($entry->isNewRecord ? 'Add' : 'Save'); ?>
-	</section>
-	<?php $this->endWidget(); ?>
-</div>
+		</section>
 </div>
