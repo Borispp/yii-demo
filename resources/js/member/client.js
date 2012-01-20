f6fdd6959b23b6b121eeb75de80489376c0cf14f
Sergey Beresnev <keriat@Sergeys-MacBook-Pro.local>
2012-01-20 18:11:35 +0200

    Notification client fancybox

--- resources/js/member/client.js
+++ resources/js/member/client.js
@@ -11,9 +11,9 @@ jQuery(function($){
 					'client_id':	$('#user-events').attr('rel').replace('client-', '')
 				}
 			}).done(function(data)
-			{
-				return;
-			});
+					{
+						return;
+					});
 		},
 		out:  function(event, ui)
 		{
@@ -26,9 +26,93 @@ jQuery(function($){
 					'client_id':	$('#user-events').attr('rel').replace('client-', '')
 				}
 			}).done(function(data)
-			{
-				return;
-			});
+					{
+						return;
+					});
 		}
 	});
+
+	$('#send-push-link').fancybox({
+		fitToView:true,
+		autoSize:false,
+		minWidth:720,
+		minHeight:400,
+		margin:40,
+		padding:0,
+		type:'ajax'
+	});
 });
+
+//
+//jQuery(function($){
+//	var clientsInput = $('#ApplicationNotification_clients');
+//	var eventsInput = $('#ApplicationNotification_events');
+//
+//	function getArrayFromString(value)
+//	{
+//		var valueArray = [];
+//		if (value)
+//		{
+//			if (value.search(',') == -1)
+//				valueArray.push(value);
+//			else
+//			{
+//				var valueTempArray = value.split(',');
+//				for(x in valueTempArray)
+//				{
+//					valueArray.push(valueTempArray[x]);
+//				}
+//			}
+//		}
+//		return valueArray;
+//	}
+//
+//	function removeFromInput(value, obInput)
+//	{
+//		var currentValues = getArrayFromString(obInput.val())
+//		if ($.inArray(value, currentValues) == -1)
+//			return;
+//		var x;
+//		var newValues = [];
+//		for(x in currentValues)
+//		{
+//			if (value == currentValues[x])
+//				continue;
+//			newValues.push(currentValues[x]);
+//		}
+//		obInput.val(newValues.join(','));
+//	}
+//
+//	function addToInput(value, obInput)
+//	{
+//		var currentValues = getArrayFromString(obInput.val())
+//		if ($.inArray(value,currentValues) != -1)
+//			return;
+//		currentValues.push(value);
+//		obInput.val(currentValues.join(','));
+//	}
+//
+//	$('.event, .client').draggable();
+//	$('#notification-events').droppable({
+//		drop: function( event, ui ) {
+//			if (ui.draggable.hasClass('event'))
+//				addToInput(ui.draggable.attr('id').replace('event-', ''), eventsInput);
+//		},
+//		out:  function(event, ui)
+//		{
+//			if (ui.draggable.hasClass('event'))
+//				removeFromInput(ui.draggable.attr('id').replace('event-', ''), eventsInput);
+//		}
+//	});
+//	$('#notification-clients').droppable({
+//		drop: function( event, ui ) {
+//			if (ui.draggable.hasClass('client'))
+//				addToInput(ui.draggable.attr('id').replace('client-', ''), clientsInput);
+//		},
+//		out:  function(event, ui)
+//		{
+//			if (ui.draggable.hasClass('client'))
+//				removeFromInput(ui.draggable.attr('id').replace('client-', ''), clientsInput);
+//		}
+//	});
+//});
