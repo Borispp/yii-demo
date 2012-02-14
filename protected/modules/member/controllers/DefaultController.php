8259a8621aadfb6278872291964c34fea0aaa1bc
George Mihailov <mihailoff@gmail.com>
2012-02-14 12:31:31 +0200

    Zendesk functionality is not stable

--- protected/modules/member/controllers/DefaultController.php
+++ protected/modules/member/controllers/DefaultController.php
@@ -3,8 +3,8 @@ class DefaultController extends YsaMemberController
 {
 	public function actionIndex()
 	{
-		$zd = new Zendesk;
-		$requests = $zd->requests($this->member()->email);
+//		$zd = new Zendesk;
+//		$requests = $zd->requests($this->member()->email);
 		
 		$this->setMemberPageTitle(Yii::t('title', 'member_area'));
 		$this->render('index', array(
