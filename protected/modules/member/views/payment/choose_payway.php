1729bc35cc2dbe93580c3d2478f381b4b3c78d7d
Eugen Figursky <eugen@flosites.com>
2012-02-16 16:25:30 +0200

    added design for payment page.

--- protected/modules/member/views/payment/choose_payway.php
+++ protected/modules/member/views/payment/choose_payway.php
@@ -5,26 +5,26 @@
 		</div>
 		<?php //@todo make new class specially for payment?>
 		<div class="box-content subscription-list">
-			<section class="part shadow-box">
+			<section class="part shadow-box fl paypal">
 				<?php echo YsaHtml::form(array('paypal/process/'))?>
 				<?php echo YsaHtml::hiddenField('type', $type)?>
 				<?php echo YsaHtml::hiddenField('summ', $summ)?>
 				<?php echo YsaHtml::hiddenField('item_id', $item_id)?>
 				<h3>Paypal</h3>
-				<p>PayPal is an American-based global e-commerce business allowing payments and
+				<p class="info">PayPal is an American-based global e-commerce business allowing payments and
 				   money transfers to be made through the Internet.</p>
 				<div class="button">
 					<?php echo YsaHtml::submitButton('Use'); ?>
 				</div>
 				<?php echo YsaHtml::endForm()?>
 			</section>
-			<section class="part shadow-box">
+			<section class="part shadow-box fr authorize">
 				<?php echo YsaHtml::form(array('authorizedotnet/process/'))?>
 				<?php echo YsaHtml::hiddenField('type', $type)?>
 				<?php echo YsaHtml::hiddenField('summ', $summ)?>
 				<?php echo YsaHtml::hiddenField('item_id', $item_id)?>
 				<h3>Authorize.net</h3>
-				<p>Since 1996, Authorize.Net has been a leading provider of payment gateway services, managing the
+				<p class="info">Since 1996, Authorize.Net has been a leading provider of payment gateway services, managing the
 				   submission of billions of transactions.</p>
 				<div class="button">
 					<?php echo YsaHtml::submitButton('Use'); ?>
