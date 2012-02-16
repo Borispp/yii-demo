d8a909a6cc0f48f2f5ebf1a346f9be7cb3d3d0db
Eugen Figursky <eugen@flosites.com>
2012-02-16 15:21:48 +0200

    added support index design.

--- protected/modules/member/views/support/index.php
+++ protected/modules/member/views/support/index.php
@@ -4,12 +4,26 @@
 			<h3><?php echo Yii::t('title', 'support'); ?></h3>
 		</div>
 		<div class="box-content cf">
-			<div class="shadow-box fl">
-				<?php echo YsaHtml::link('Have a question?', array('help/')); ?>
+			<div class="shadow-box fl tutorials">
+				<a href="<?php echo Yii::app()->createUrl('member/help/')?>">
+					<strong>
+						<?php echo Yii::t('title', 'Tutorials'); ?>
+					</strong>
+					<em>
+						<?php echo Yii::t('support', 'support_tutorials_caption'); ?>
+					</em>
+				</a>				
 			</div>
 			
-			<div class="shadow-box fr">
-				<?php echo YsaHtml::link('Have a question?', Yii::app()->settings->get('zendesk_url'), array('rel' => 'external')); ?>
+			<div class="shadow-box fr helpdesk">
+				<a href="<?php echo Yii::app()->settings->get('zendesk_url')?>" rel="external">
+					<strong>
+						<?php echo Yii::t('title', 'Helpdesk'); ?>
+					</strong>
+					<em>
+						<?php echo Yii::t('support', 'support_helpdesk_caption'); ?>
+					</em>
+				</a>
 			</div>
 		</div>
 	</section>
