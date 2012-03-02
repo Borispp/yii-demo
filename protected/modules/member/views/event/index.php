1fc559128328553659029bd392e2a2a3c61fea13
Eugen Figursky <eugen@flosites.com>
2012-03-02 16:50:50 +0200

    added some improvements to member area.

--- protected/modules/member/views/event/index.php
+++ protected/modules/member/views/event/index.php
@@ -10,9 +10,10 @@
 			'searchOptions' => $searchOptions,
 		));?>
 		<div class="data-box shadow-box">
-			<table class="data">
+			<table class="data" data-control="event">
 				<thead>
 					<tr>
+						<th class="w_1"><input type="checkbox" value="all" name="entries-select-all" id="entries-select-all" /></th>
 						<th class="w_1">ID</th>
 						<th class="w_30">Name</th>
 						<th>Description</th>
@@ -21,15 +22,25 @@
 						<th class="actions">Actions</th>
 					</tr>
 				</thead>
+				<?php if (count($entries)) : ?>
+					<tfoot>
+						<tr>
+							<td colspan="7">
+								<?php echo YsaHtml::link('Remove Selected', '#', array('class' => 'btn small entries-delete-selected gray-txt')); ?>
+							</td>
+						</tr>
+					</tfoot>
+				<?php endif;?>
 				<tbody>
 					<?php if (count($entries)) : ?>
 						<?php foreach ($entries as $entry) : ?>
-								<tr class="state-<?php echo strtolower($entry->state()); ?>">
+								<tr class="state-<?php echo strtolower($entry->state()); ?>" id="entry-row-<?php echo $entry->id?>">
+									<td><input type="checkbox" name="entries[<?php echo $entry->id?>]" value="<?php echo $entry->id?>" /></td>
 									<td><?php echo $entry->id; ?></td>
 									<td>
 										<?php echo YsaHtml::link('<strong>' . $entry->name . '</strong>', array('event/view/' . $entry->id), array('class' => 'title')); ?>
 										<?php if (!$entry->isPortfolio()) : ?>
-											<span class="descr">Password: <strong><?php echo $entry->passwd; ?></strong></span>
+											<span class="descr">ID: <strong><?php echo $entry->id; ?></strong>&nbsp;&nbsp;|&nbsp;&nbsp;Password: <strong><?php echo $entry->passwd; ?></strong></span>
 										<?php endif; ?>
 									</td>
 									<td>
